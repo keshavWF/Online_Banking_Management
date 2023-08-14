03060cd634e9db9873799a4905809f0e5056a99b
Prateek-ship-it <141805751+Prateek-ship-it@users.noreply.github.com>
2023-08-14 13:29:53 +0530

    Adding Payee file and adding one to many relation

--- app/model/Account.java
+++ app/model/Account.java
@@ -1,24 +1,36 @@
-package com.banking.app.model;
+package com.banking.app;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.OneToMany;
 
 @Entity
-@Table
+@Table(name = "account")
 public class Account {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int acc_number;
 	@Column
 	private double acc_balance;
-	@Column
+	@Column(name = "userid")
 	private int userid;
 	@Column
 	private String acc_type;
 	
+	@JsonManagedReference
+    @OneToMany(mappedBy = "account",
+    		 cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+                     CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Payee> payees;
+//	
 	public Account() {
 		
 	}
@@ -64,3 +76,5 @@ public class Account {
 	
 	
 }
+
+
---
+++ app/model/Payee.java
@@ -0,0 +1,95 @@
+package com.banking.app;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "payee")
+public class Payee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int payee_id;
+	@Column
+	private String FirstName;
+	@Column
+	private String LastName;
+	@Column
+	private String NickName;
+	@Column
+	private int AccountNumber;
+//	@Column
+//	private int userid;
+	
+	@JsonBackReference
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH })
+	@JoinColumn(name = "userid")
+	private Account account;
+	
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	private Account account;
+//	
+	public Payee() {
+		
+	}
+
+	public Payee(int payee_id, String FirstName, String LastName, String NickName, int AccountNumber, int userid) {
+		super();
+		this.payee_id = payee_id;
+		this.FirstName = FirstName;
+		this.LastName = LastName;
+		this.AccountNumber = AccountNumber;
+		this.userid = userid;
+	}
+
+	public int getPayee_id() {
+		return payee_id;
+	}
+
+	public void setPayee_id(int payee_id) {
+		this.payee_id = payee_id;
+	}
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.FirstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.LastName = lastName;
+	}
+
+	public String getNickName() {
+		return NickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.NickName = nickName;
+	}
+
+	public int getAccountNumber() {
+		return AccountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.AccountNumber = accountNumber;
+	}
+	
+}
+
