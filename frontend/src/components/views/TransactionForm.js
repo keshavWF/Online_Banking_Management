c6e67cbfcb1853a5962e7035135a5e7e27f37fc9
keshav <keshav.singhal@wellsfargo.com>
2023-08-23 15:05:25 +0530

    transaction page updated

--- frontend/src/components/views/TransactionForm.js
+++ frontend/src/components/views/TransactionForm.js
@@ -1,29 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-import axios from "axios";
+import axios from "../../utilities/axios";
+import { useHistory } from "react-router";
+const PAYEE_URL = "/payee/getPayee/";
+const ACC_URL = "/account/getAccount/";
+const PAYEE_ACC_URL = "/account/getAccount";
 
-const samplePayees = [
-  { id: 1, name: "Payee 1" },
-  { id: 2, name: "Payee 2" },
-  { id: 3, name: "Payee 3" },
-];
+// const samplePayees = [
+//   { id: 1, name: "Payee 1" },
+//   { id: 2, name: "Payee 2" },
+//   { id: 3, name: "Payee 3" },
+// ];
 
 const PaymentForm = () => {
-  const [payees, setPayees] = useState(samplePayees);
+  const history = useHistory();
+  const [payees, setPayees] = useState([]);
+  const [accounts, setAccounts] = useState([]);
+  const [selectedAccount, setSelectedAccount] = useState("");
   const [selectedPayee, setSelectedPayee] = useState("");
+  const [PayeeUserName, setPayeeUsername] = useState("");
   const [amount, setAmount] = useState("");
   const [transactionMode, setTransactionMode] = useState("");
 
+  useEffect(() => {
+    axios
+      .get(PAYEE_URL + sessionStorage.getItem("username"))
+      .then((response) => {
+        setPayees(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+  useEffect(() => {
+    axios
+      .get(ACC_URL + sessionStorage.getItem("username"))
+      .then((response) => {
+        setAccounts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     // Get userName from sessionStorage
-    const userName = sessionStorage.getItem("userName");
-
+    const fromUserName = sessionStorage.getItem("userName");
+    const res = await axios.get(
+      PAYEE_ACC_URL + "/" + selectedPayee,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Aloow-Headers": "Content-Type",
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, OPTIONS, PUT, DELETE",
+        },
+        withCredentials: false,
+      }
+    );
+    setPayeeUsername(res.data);
     // Create the data object for POST request
     const paymentData = {
-      userName,
-      payee: selectedPayee,
+      fromUserName,
+      toUserName: PayeeUserName,
+      fromAccountNumber: selectedAccount,
+      toAccountNumber: selectedPayee,
       amount,
       transactionMode,
       timestamp: new Date().toISOString(),
@@ -31,12 +74,13 @@ const PaymentForm = () => {
 
     try {
       // Make POST request to submit payment (replace with your API endpoint)
-      await axios.post("your_payment_api_endpoint", paymentData);
+      // await axios.post("your_payment_api_endpoint", paymentData);
       alert("Payment submitted successfully!");
       // Clear form fields after successful submission
-      setSelectedPayee("");
-      setAmount("");
-      setTransactionMode("");
+      // setSelectedPayee("");
+      // setAmount("");
+      // setTransactionMode("");
+      history.push("/dashboard");
     } catch (error) {
       console.error("Error submitting payment:", error);
       alert("Error submitting payment. Please try again.");
@@ -57,8 +101,24 @@ const PaymentForm = () => {
           >
             <option value="">Select a payee</option>
             {payees.map((payee) => (
-              <option key={payee.id} value={payee.id}>
-                {payee.name}
+              <option key={payee.payeeID} value={payee.accountNumber}>
+                {payee.nickName}
+              </option>
+            ))}
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for="accSelect">Select Your Account : </Label>
+          <Input
+            type="select"
+            id="accSelect"
+            value={selectedAccount}
+            onChange={(e) => setSelectedAccount(e.target.value)}
+          >
+            <option value="">Select a account</option>
+            {accounts.map((acc) => (
+              <option key={acc.accountNumber} value={acc.accountNumber}>
+                {acc.accountNumber}
               </option>
             ))}
           </Input>
