e80383fa095c0927cc2486196afe3e4009033cc6
unknown <shivam.singh@wellsfargo.com>
2023-08-24 09:30:07 +0530

    updating user dashboard

--- frontend/src/components/views/UserDashboard.js
+++ frontend/src/components/views/UserDashboard.js
@@ -3,7 +3,12 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import AccountCreation from "./AccountCreation";
 import AddPayee from "./AddPayee";
 import { Switch } from "react-router-dom/cjs/react-router-dom.min";
-import {
+import AccountDetails from "./AccountDetails";
+import TransactionForm from "./TransactionForm";
+import ViewTransactions from "./ViewTransactions";
+import Sidebar from "../Sidebar";
+/*import {
+  Row, Col, 
   Button,
   Card,
   CardBody,
@@ -11,98 +16,22 @@ import {
   CardSubtitle,
   CardText,
 } from "reactstrap";
-import { Row, Col } from "reactstrap";
-import AccountDetails from "./AccountDetails";
-import TransactionForm from "./TransactionForm";
-import ViewTransactions from "./ViewTransactions";
-//
+const cardStyle = { width: "16rem" };*/
+
 const UserDashboard = () => {
   return (
     <div className="row g-0 auth-wrapper">
-      <div className="col-12 col-lg-12 auth-main-col text-center">
-        {/* <div className="d-flex flex-column align-content-flex-start">
-                    <div className="auth-body mx-auto">
-                        <h2>Bank</h2>
-                    </div>
-                </div> */}
+      <Sidebar className= "col-lg-4"/>
+      
+      <div className="col-12 col-lg-8 auth-main-col text-center">
+      <AccountDetails/>
         <Router>
           <Switch>
             <Route path="/user/account-creation" component={AccountCreation} />
             <Route path="/user/add-payee" component={AddPayee} />
             <Route path="/user/account-details" component={AccountDetails} />
             <Route path="/user/transaction-form" component={TransactionForm} />
-            <Route
-              path="/user/view-transactions"
-              component={ViewTransactions}
-            />
-            <div>
-              <Row>
-                <Col className=".container-fluid">
-                  <Card style={{ width: "16rem" }}>
-                    <img alt="Sample" src="https://picsum.photos/300/200" />
-                    <CardBody>
-                      <CardTitle tag="h5">Account Details</CardTitle>
-                      <CardText>View Existing Accounts</CardText>
-                      <Link className="mb-3" to="./user/account-details">
-                        <Button>Button</Button>
-                      </Link>
-                    </CardBody>
-                  </Card>
-                </Col>
-                <Col className=".container-fluid">
-                  <Card style={{ width: "16rem" }}>
-                    <img alt="Sample" src="https://picsum.photos/300/200" />
-                    <CardBody>
-                      <CardTitle tag="h5">Create account</CardTitle>
-                      <CardText>
-                        Create a new savings or checking account
-                      </CardText>
-                      <Link className="mb-3" to="/user/account-creation">
-                        <Button>Button</Button>
-                      </Link>
-                    </CardBody>
-                  </Card>
-                </Col>
-                <Col className=".container-fluid">
-                  <Card style={{ width: "16rem" }}>
-                    <img alt="Sample" src="https://picsum.photos/300/200" />
-                    <CardBody>
-                      <CardTitle tag="h5">Add Payee</CardTitle>
-                      <CardText>Add New Payees</CardText>
-                      <Link className="mb-3" to="/user/add-payee">
-                        <Button>Button</Button>
-                      </Link>
-                    </CardBody>
-                  </Card>
-                </Col>
-                <Col className=".container-fluid">
-                  <Card style={{ width: "16rem" }}>
-                    <img alt="Sample" src="https://picsum.photos/300/200" />
-                    <CardBody>
-                      <CardTitle tag="h5">Send Money</CardTitle>
-                      <CardText>Send Money to Added Payees</CardText>
-                      <Link className="mb-3" to="/user/transaction-form">
-                        <Button>Button</Button>
-                      </Link>
-                    </CardBody>
-                  </Card>
-                </Col>
-                <Col className=".container-fluid">
-                  <Card style={{ width: "16rem" }}>
-                    <img alt="Sample" src="https://picsum.photos/300/200" />
-                    <CardBody>
-                      <CardTitle tag="h5">Recent Transactions</CardTitle>
-                      <CardText>
-                        View your recent NEFT, RTGS and IMPS Transactions
-                      </CardText>
-                      <Link className="mb-3" to="/user/view-transactions">
-                        <Button>Button</Button>
-                      </Link>
-                    </CardBody>
-                  </Card>
-                </Col>
-              </Row>
-            </div>
+            <Route path="/user/view-transactions" component={ViewTransactions} />
           </Switch>
         </Router>
       </div>
@@ -111,3 +40,69 @@ const UserDashboard = () => {
 };
 
 export default UserDashboard;
+
+/*
+  <div>
+    <Row>
+      <Col className=".container-fluid">
+        <Card {...cardStyle}>
+          <CardBody>
+            <CardTitle tag="h5">Account Details</CardTitle>
+            <CardText>View Existing Accounts</CardText>
+            <Link className="mb-3" to="./user/account-details">
+              <Button>Button</Button>
+            </Link>
+          </CardBody>
+        </Card>
+      </Col>
+      <Col className=".container-fluid">
+        <Card {...cardStyle}>
+          <CardBody>
+            <CardTitle tag="h5">Create account</CardTitle>
+            <CardText>
+              Create a new savings or checking account
+            </CardText>
+            <Link className="mb-3" to="/user/account-creation">
+              <Button>Button</Button>
+            </Link>
+          </CardBody>
+        </Card>
+      </Col>
+      <Col className=".container-fluid">
+        <Card {...cardStyle}>
+          <CardBody>
+            <CardTitle tag="h5">Add Payee</CardTitle>
+            <CardText>Add New Payees</CardText>
+            <Link className="mb-3" to="/user/add-payee">
+              <Button>Button</Button>
+            </Link>
+          </CardBody>
+        </Card>
+      </Col>
+      <Col className=".container-fluid">
+        <Card {...cardStyle}>
+          <CardBody>
+            <CardTitle tag="h5">Send Money</CardTitle>
+            <CardText>Send Money to Added Payees</CardText>
+            <Link className="mb-3" to="/user/transaction-form">
+              <Button>Button</Button>
+            </Link>
+          </CardBody>
+        </Card>
+      </Col>
+      <Col className=".container-fluid">
+        <Card {...cardStyle}>
+          <CardBody>
+            <CardTitle tag="h5">Recent Transactions</CardTitle>
+            <CardText>
+              View your recent NEFT, RTGS and IMPS Transactions
+            </CardText>
+            <Link className="mb-3" to="/user/view-transactions">
+              <Button>Button</Button>
+            </Link>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+  </div>
+*/
